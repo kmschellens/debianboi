a0b95f7f4435d68474cb739f2d7a3367b88b12d5
Koen Schellens <k.schellens@gmail.com>
2025-06-17 22:47:02 +0200

    init

---
+++ ansible/start.sh
@@ -0,0 +1,5 @@
+# ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/docker-install.yml
+# ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/dashboard.yml
+
+# ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/webserver.yml
+ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/docker.yml
