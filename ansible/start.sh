c1d99d46e23bef043f6bf669892843d10e07bb27
Koen Schellens <k.schellens@gmail.com>
2025-09-10 00:11:00 +0200

    incl authentik

--- ansible/start.sh
+++ ansible/start.sh
@@ -1,5 +1,5 @@
 # ansible-playbook -i hosts playbooks/enable-docker.yml
 # ansible-playbook -i hosts playbooks/dashboard.yml
 
-# ansible-playbook -i hosts playbooks/webserver.yml
+ansible-playbook -i hosts playbooks/webserver.yml
 ansible-playbook -i hosts playbooks/docker.yml
